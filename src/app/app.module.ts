53adb8f0965778701256f1492321352ce5b3604f
Arcerr <taurus..ali47>
2022-01-23 14:53:09 +0200

    header works , register/login popup window works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { LoginRegisterPopupComponent } from './login-register-popup/login-register-popup.component';
+import { LoginWindowComponent } from './login-window/login-window.component';
+import { RegisterWindowComponent } from './register-window/register-window.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    LoginRegisterPopupComponent,
+    LoginWindowComponent,
+    RegisterWindowComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,HeaderComponent,FooterComponent]
 })
 export class AppModule { }
